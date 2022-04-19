5b96b5e7d0c2871a84392012d48f13b785dda318
Zihadul Islam <zihadul10101@gmail.com>
2022-04-19 22:28:46 +0600

    about updated

--- components/section/About.js
+++ components/section/About.js
@@ -6,7 +6,7 @@ import { FaDownload } from 'react-icons/fa';
 const About = () => {
 
     return (
-        <section id="about" className="border border-red-500 text-gray-600 body-font">
+        <section id="about" className="border border-sate-900 text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto ">
                 <div className=" w-full mb-10">
                     <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
@@ -25,14 +25,13 @@ const About = () => {
                         </div>
                         <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                             <p className="leading-relaxed text-lg mb-4">
-                                A deep heart javaScript lover from Bangladesh
-                                <br />
-                                A self-motivated and enthusiastic web developer with a deep interest in JavaScript. I primarily focus on writing clean, elegant, and eﬃcient code. I am proﬁcient in HTML, CSS, Bootstrap, JavaScript, React, Mongo DB, Node Js, Express Js, Heroku, Firebase, and Netlify . I am always determined to learn & explore new technology.
+                                I am a Front-end developer Always ready to prove myself through my work and responsibility. I specialize in JavaScript and My role is Frontend Developer || React js Developer || MERN Stack, Developer
                             </p>
+                            <p >My specialties include quickly learning new skills and programming languages, problem-solving, responsive design principles, website optimization, and Model View Controller (MVC) methods of organizing code. So far I have JavaScript, React js, HTML, CSS, Bootstrap, Tailwindcss, and Git/GitHub under my belt. I have started learning Node.JS, MongoDB, and Express.JS. I am still enthusiastically grabbing onto any other programming languages, frameworks, or principles I can integrate into the coding web in my head.</p>
                             <button
-                                className="flex items-center ml-5 animate-bounce bg-green-500  px-3 py-2 text-lg uppercase text-white rounded-lg mt-10 hover:bg-green-700 transform"
+                                className="flex items-center ml-5 animate-bounce bg-green-500  px-3 py-2 text-lg  text-slate-800 font-medium rounded-lg mt-10 hover:bg-green-700 transform"
                             >
-                                <FaDownload className="text-white mr-3" />
+                                <FaDownload className="text-slate-800 mr-3" />
                                 Download Resume
                             </button>
                         </div>
