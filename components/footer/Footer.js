e605767b1de5bb375a24a18623e55ac02cf412e7
Zihadul Islam <zihadul10101@gmail.com>
2022-04-23 17:56:22 +0600

    about updated

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer style={{"textAlign":"center"}}><p>{`Resume © ${new Date().getFullYear()}  Created by Zihadul Islam`}</p></footer>
+        <footer className='text-center'>
+            <p className='text-xl'>{`Resume © ${new Date().getFullYear()}  Created by Zihadul Islam`}</p>
+            </footer>
     );
 };
 
--- components/section/About.js
+++ components/section/About.js
@@ -24,14 +24,14 @@ const About = () => {
 
                         </div>
                         <div data-aos="fade-left" className="sm:w-2/3 sm:pl-8 sm:py-8  border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                            <p className="leading-relaxed font-medium text-lg mb-4">
+                            <p className="leading-relaxed font-medium text-lg mb-6">
                                 I am a Front-end developer Always ready to prove myself through my work and responsibility. I specialize in JavaScript and My role is Frontend Developer || React js Developer || MERN Stack, Developer
                             </p>
-                            <p >My specialties include quickly learning new skills and programming languages, problem-solving, responsive design principles, website optimization, and Model View Controller (MVC) methods of organizing code. So far I have JavaScript, React js, HTML, CSS, Bootstrap, Tailwindcss, and Git/GitHub under my belt. I have started learning Node.JS, MongoDB, and Express.JS. I am still enthusiastically grabbing onto any other programming languages, frameworks, or principles I can integrate into the coding web in my head.</p>
+                            <p className="mb-6">My specialties include quickly learning new skills and programming languages, problem-solving, responsive design principles, website optimization, and Model View Controller (MVC) methods of organizing code. So far I have JavaScript, React js, HTML, CSS, Bootstrap, Tailwindcss, and Git/GitHub under my belt. I have started learning Node.JS, MongoDB, and Express.JS. I am still enthusiastically grabbing onto any other programming languages, frameworks, or principles I can integrate into the coding web in my head.</p>
                             <button
-                                className="flex items-center ml-5 animate-bounce bg-green-500  px-3 py-2 text-lg  text-slate-800 font-medium rounded-lg mt-10 hover:bg-green-700 transform"
+                                className="flex items-center  border-2 border-green-500 px-3 py-2 text-slate-600 uppercase font-medium hover:bg-green-600 hover:text-white "
                             >
-                                <FaDownload className=" text-white mr-3" />
+                                <FaDownload className="text-slate-500 mr-3" />
                                 Download Resume
                             </button>
                         </div>
